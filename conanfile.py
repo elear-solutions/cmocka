0256073e85569c807122a2c982cb6a2a6b28cc73
rohan-elear <53178851+rohan-elear@users.noreply.github.com>
2020-01-29 14:14:55 +0530

    fix unix builds for macOS and iOS (#3)
    * fix conanfile with version

--- conanfile.py
+++ conanfile.py
@@ -2,21 +2,24 @@ from conans import ConanFile, CMake, tools
 
 class cmockalibConan(ConanFile):
     name = "cmocka"
-    version = "0.1"
+    version = "0.4.1"
     license = "<Put the package license here>"
     author = "<Put your name here> <And your email here>"
-    url = "<Package recipe repository url here, for issues about the package>"
-    description = "<Description of cmockalib here>"
-    topics = ("<Put some tag here>", "<here>", "<and here>")
+    url = "https://github.com/elear-solutions/cmocka"
+    homepage = "https://git.cryptomilk.org/projects/cmocka.git"
+    description = "Unit testing framework"
+    topics = ("c", "unit", "testing")
     settings = "os", "compiler", "build_type", "arch"
-    options = { "shared": [True, False] }
-    default_options = "shared=False"
     generators = "cmake"
+    options = {
+        "shared": [True, False]
+    }
+    default_options = {
+        "shared": False
+    }
 
     def build(self):
         cmake = CMake(self)
-        if (self.settings.os == "Android"):
-            cmake.definitions["Platform"] = "android"
         cmake.configure(source_folder=".")
         cmake.build()
 
